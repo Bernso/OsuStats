624e11fbe6d4cadc3640ad74310dda85953fbee6
Bernso <ben.w.sheridan@gmail.com>
2024-08-18 21:23:55 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from osuapi import OsuApi, ReqConnector; import os; from dotenv import load_dotenv
+
+load_dotenv()
+apiKey = os.getenv("API_KEY")
+
+# Initialize API
+api = OsuApi(apiKey, connector=ReqConnector())
+
+userUsernameInput = input("Enter the user of the stats you want to see:")
+# User to get stats for
+user = api.get_user(username=userUsernameInput)
+
+# Display some stats
+print(f"User: {user[0].username}")
+print(f"Rank: {user[0].pp_rank}")
+print(f"Play Count: {user[0].playcount}")
+print(f"Accuracy: {user[0].accuracy}")
