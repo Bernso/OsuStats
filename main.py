4aa3c217c529b7268c4a63f4ad607e529609cdec
Bernso <ben.w.sheridan@gmail.com>
2024-08-18 21:24:10 +0100

    yup

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ apiKey = os.getenv("API_KEY")
 api = OsuApi(apiKey, connector=ReqConnector())
 
 userUsernameInput = input("Enter the user of the stats you want to see:")
-# User to get stats for
+# User to get stats for x
 user = api.get_user(username=userUsernameInput)
 
-# Display some stats
+# Display stats
 print(f"User: {user[0].username}")
 print(f"Rank: {user[0].pp_rank}")
 print(f"Play Count: {user[0].playcount}")
